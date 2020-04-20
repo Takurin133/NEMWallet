ca11c5ddf63dc946d356ec4e6f3cf4d0dccde08c
daoka <daoka.sweep@gmail.com>
2020-04-20 16:25:24 +0900

    QRリーダの導入

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  isOpen = false;
+  opacity = 1;
 
-  constructor() {}
+  constructor(
+    public barcodeScanner: BarcodeScanner,
+  ) {}
+
+  runQRScanner() {
+    this.barcodeScanner.scan().then((barcodeData) => {
+      console.log(barcodeData);
+    }).catch((e) => {
+      console.error(e);
+    });
+  }
 
 }
