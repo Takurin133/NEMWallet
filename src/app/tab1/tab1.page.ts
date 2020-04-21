39635dc60d5e30ae1faa66d51a54c40bee55b77d
daoka <daoka.sweep@gmail.com>
2020-04-21 18:32:31 +0900

    QRコードの読み込みから支払いまでのつなぎ込みを実装

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { BarcodeScanner, BarcodeScanResult } from '@ionic-native/barcode-scanner/ngx';
+import { LoadingController } from '@ionic/angular';
 import { Account, NetworkType, TransferTransaction, Deadline,
-   Address, NetworkCurrencyPublic, PlainMessage, UInt64,
-    PublicAccount, AggregateTransaction, HashLockTransaction, TransactionService, RepositoryFactoryHttp, Listener } from 'symbol-sdk';
+  Address, NetworkCurrencyPublic, UInt64,
+  PublicAccount, AggregateTransaction, HashLockTransaction,
+  TransactionService, RepositoryFactoryHttp, Listener, NamespaceId } from 'symbol-sdk';
+
 
 @Component({
   selector: 'app-tab1',
@@ -12,25 +15,91 @@ import { Account, NetworkType, TransferTransaction, Deadline,
 export class Tab1Page {
   isOpen = false;
   opacity = 1;
+  symbolQrData: {
+    v: number,
+    network_id: number,
+    chain_id: number,
+    data: { payload: string }
+  };
+  transferTx: TransferTransaction;
 
   constructor(
     public barcodeScanner: BarcodeScanner,
+    public loadingControler: LoadingController,
   ) {}
 
   runQRScanner() {
-    this.barcodeScanner.scan().then((barcodeData) => {
-      console.log(barcodeData);
+    this.barcodeScanner.scan().then((barcodeData: BarcodeScanResult) => {
+      if (!barcodeData.cancelled && barcodeData.format === 'QR_CODE') {
+        const payload = this.parseQRJSON(barcodeData.text);
+        if (payload) {
+          this.parsePayload(payload);
+        }
+      }
     }).catch((e) => {
       console.error(e);
     });
   }
 
+  parseQRJSON(barcodeData: string): string {
+    this.symbolQrData = JSON.parse(barcodeData);
+    if (this.symbolQrData?.data?.payload) {
+      console.log(this.symbolQrData.data.payload);
+      return this.symbolQrData.data.payload;
+    }
+    return null;
+  }
 
-  sendSymMock() {
+  parsePayload(payload) {
+    const tx = TransferTransaction.createFromPayload(payload);
+    if (tx.type === 0x4154) {
+      this.transferTx = tx as TransferTransaction;
+      console.log(this.transferTx);
+    }
+  }
+
+  recipientAddress(): string {
+    if (!this.transferTx) {
+      return '';
+    }
+    if (this.transferTx.recipientAddress instanceof Address) {
+      return this.transferTx.recipientAddress.pretty();
+    }
+
+    if (this.transferTx.recipientAddress instanceof NamespaceId) {
+      return this.transferTx.recipientAddress.fullName;
+    }
+
+    return '';
+  }
+
+  payAmount(): number {
+    let amount: number = null;
+    if (this.transferTx) {
+      const sym = this.transferTx.mosaics.find((m) => {
+        return m.id.id.toHex() === '747B276C30626442';
+      });
+      if (sym) {
+        const absolute = Number(sym.amount.toString());
+        amount = absolute * Math.pow(10, -NetworkCurrencyPublic.DIVISIBILITY);
+      }
+    }
+    return amount;
+  }
+
+  message(): string {
+    if (!this.transferTx) {
+      return '';
+    }
+
+    return this.transferTx.message.payload;
+  }
+
+
+  async payXym() {
     const cosignatoryPrivateKey = '8458373A2CC9FC919D1E339E0D21B3313C7460EC2EC28875E53E8CA9A8B79B31';
     const multisigPublicKey = '2591ACAE34EE6044C4197DCC763DAC20C5CDEAE09DB5E32AB72DC4B5B943AB8A';
     const genHash = '44D2225B8932C9A96DCB13508CBCDFFA9A9663BFBA2354FEEC8FCFCB7E19846C';
-    const recipient = 'TAS3GLDV4JM5AEXNLA5K5W27CP2UEPO6NDAOCSOT';
 
     const endPoint = 'https://sym-test.opening-line.jp:3001';
     const wsEndpoint = endPoint.replace('http', 'ws');
@@ -44,17 +113,10 @@ export class Tab1Page {
 
     const cosignatory = Account.createFromPrivateKey(cosignatoryPrivateKey, networkType);
     const multisig = PublicAccount.createFromPublicKey(multisigPublicKey, networkType);
-    const transferTx = TransferTransaction.create(
-      Deadline.create(),
-      Address.createFromRawAddress(recipient),
-      [NetworkCurrencyPublic.createRelative(100)],
-      PlainMessage.create('hello multisig'),
-      networkType,
-    );
 
     const aggregateTx = AggregateTransaction.createBonded(
       Deadline.create(),
-      [transferTx.toAggregate(multisig)],
+      [this.transferTx.toAggregate(multisig)],
       networkType,
     ).setMaxFeeForAggregate(100, 2);
 
@@ -72,18 +134,31 @@ export class Tab1Page {
 
     const transactionService = new TransactionService(transactionRep, receiptRep);
 
+    const loading = await this.loadingControler.create({
+      message: 'しはらいちゅう',
+    });
+
+    await loading.present();
+
     listener.open().then(() => {
       transactionService.announceHashLockAggregateBonded(signedHashLockTx, signedAggregateTx, listener).subscribe((x) => {
         console.log(x);
-        listener.close();
+        this.resetPayStatus(listener, loading);
       }, (err) => {
         console.error(err);
-        listener.close();
+        this.resetPayStatus(listener, loading);
       });
     }).catch((err) => {
       console.error(err);
+      this.transferTx = null;
+      loading.dismiss();
     });
+  }
 
+  resetPayStatus(listener: Listener, loading: HTMLIonLoadingElement) {
+    listener.close();
+    this.transferTx = null;
+    loading.dismiss();
   }
 
 }
